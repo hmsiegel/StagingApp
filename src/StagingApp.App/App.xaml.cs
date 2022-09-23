3a1be7228940a6c03d91adffe52e4efc9d417377
Harlan Siegel <hmsiegel79@msn.com>
2022-09-23 14:12:32 -0400

    Updates Styles in App.xaml

--- src/StagingApp.App/App.xaml.cs
+++ src/StagingApp.App/App.xaml.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace StagingApp.App;
+﻿namespace StagingApp.Main;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
+    public IServiceProvider? ServiceProvider { get; private set; }
+    public IConfiguration? Configuration { get; private set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var shellView = ServiceProvider.GetRequiredService<ShellView>();
+        shellView.Show();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient(typeof(ShellView));
+    }
 }
