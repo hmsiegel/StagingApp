8c7ad2ef9f50b396a389dc365af2a33c13a65ba9
Harlan Siegel <hmsiegel79@msn.com>
2022-10-03 08:08:47 -0400

    Add Common ViewModel Folder

---
+++ src/StagingApp.Presentation/ViewModels/Common/DeviceInfoRowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace StagingApp.Presentation.ViewModels.Common;
+public partial class DeviceInfoRowViewModel : ObservableObject, INotifyPropertyChanged
+{
+    private string? _labelText;
+
+    public string? LabelText
+    {
+        get => _labelText;
+        set 
+        { 
+            _labelText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string? _infoTextBox;
+
+    public string? InfoTextBox
+    {
+        get => _infoTextBox;
+        set 
+        { 
+            _infoTextBox = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    [RelayCommand]
+    private void Ok()
+    {
+
+    }
+
+    [RelayCommand]
+    private void Cancel()
+    {
+        
+    }
+
+    [RelayCommand]
+    private void Edit()
+    {
+        
+    }
+}
+
