59b1d677f6648e53e7a2d85ea107b67d8a438e18
Harlan Siegel <hmsiegel79@msn.com>
2022-10-30 11:54:06 -0400

    Change ShellViewModel to use CM convention

--- src/StagingApp.Presentation/ViewModels/ShellViewModel.cs
+++ src/StagingApp.Presentation/ViewModels/ShellViewModel.cs
@@ -1,6 +1,16 @@
-﻿using Caliburn.Micro;
+﻿using StagingApp.Presentation.ViewModels.ConfigureViewModels;
 
 namespace StagingApp.Presentation.ViewModels;
 public sealed class ShellViewModel : Conductor<object>
 {
+	public ShellViewModel()
+	{
+	}
+
+	protected override async void OnViewLoaded(object view)
+	{
+		base.OnViewLoaded(view);
+		var viewModel = IoC.Get<KitchenConfigureViewModel>();
+		await ActivateItemAsync(viewModel, new CancellationToken());
+	}
 }
