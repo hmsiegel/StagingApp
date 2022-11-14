890f14e50856212f46800c370d34b27f71832bef
Harlan Siegel <hmsiegel79@msn.com>
2022-11-14 16:56:45 -0500

    Add Registry Service

---
+++ src/StagingApp.Application/Abstractions/Services/IRegistryService.cs
@@ -0,0 +1,5 @@
+﻿namespace StagingApp.Application.Abstractions.Services;
+public interface IRegistryService
+{
+    void SetRegistryKeyAndValue(string subKeyName, string name, string value);
+}
---
+++ src/StagingApp.Infrastructure/Services/RegistryService.cs
@@ -0,0 +1,12 @@
+﻿namespace StagingApp.Infrastructure.Services;
+
+[SupportedOSPlatform("Windows7.0")]
+public class RegistryService : IRegistryService
+{
+    public void SetRegistryKeyAndValue(string subKeyName, string name, string value)
+    {
+        RegistryKey registryKey = Registry.LocalMachine.CreateSubKey(subKeyName);
+        registryKey.SetValue(name, value);
+        registryKey.Close();
+    }
+}
