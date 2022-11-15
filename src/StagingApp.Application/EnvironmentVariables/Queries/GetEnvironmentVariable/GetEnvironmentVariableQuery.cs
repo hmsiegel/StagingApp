fbe24a4f5b7422cbf4e06c6b68d14230486d830a
Harlan Siegel <hmsiegel79@msn.com>
2022-11-15 16:58:58 -0500

    Adds GetEnvironmentVariableQuery and Handler

---
+++ src/StagingApp.Application/EnvironmentVariables/Queries/GetEnvironmentVariable/GetEnvironmentVariableQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace StagingApp.Application.EnvironmentVariables.Queries.GetEnvironmentVariable;
+
+public sealed record GetEnvironmentVariableQuery(string Key) : IQuery<string>;
---
+++ src/StagingApp.Application/EnvironmentVariables/Queries/GetEnvironmentVariable/GetEnvironmentVariableQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace StagingApp.Application.EnvironmentVariables.Queries.GetEnvironmentVariable;
+internal sealed class GetEnvironmentVariableQueryHandler : IQueryHandler<GetEnvironmentVariableQuery, string>
+{
+    public async Task<Result<string>> Handle(GetEnvironmentVariableQuery request, CancellationToken cancellationToken)
+    {
+        return await Task.Run(() => Environment.GetEnvironmentVariable(request.Key));
+    }
+}
