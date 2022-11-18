18c9c40b8e32bf30f6eed0fb32de1f3cbef028a0
Harlan Siegel <hmsiegel79@msn.com>
2022-11-18 15:44:49 -0500

    Convert TerminalConfigureModel to ValueObject

--- src/StagingApp.Presentation/Models/ConfigureModels/TerminalConfigureModel.cs
+++ src/StagingApp.Presentation/Models/ConfigureModels/TerminalConfigureModel.cs
@@ -1,7 +1,21 @@
 ﻿namespace StagingApp.Presentation.Models.ConfigureModels;
-public sealed class TerminalConfigureModel
+public sealed class TerminalConfigureModel : ValueObject
 {
-    public string? TerminalName { get; set; }
-    public string? IpAddress { get; set; }
+    public string TerminalName { get; private set; }
+    public string IpAddress { get; private set; }
 
+    private TerminalConfigureModel(string terminalName, string ipAddress)
+    {
+        TerminalName = terminalName;
+        IpAddress = ipAddress;
+    }
+
+    public static TerminalConfigureModel Create(string terminalName, string ipAddress) =>
+        new(terminalName, ipAddress);
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return TerminalName;
+        yield return IpAddress;
+    }
 }
