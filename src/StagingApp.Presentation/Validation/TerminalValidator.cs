51288acd3f200bbd4a1b5f6ea83de0253c86d647
Harlan Siegel <hmsiegel79@msn.com>
2022-11-18 15:46:13 -0500

    Add TemrinalValidator

---
+++ src/StagingApp.Presentation/Validation/TerminalValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace StagingApp.Presentation.Validation;
+public class TerminalValidator : AbstractValidator<TerminalConfigureModel>
+{
+    public TerminalValidator()
+    {
+        RuleFor(x => x.TerminalName)
+            .NotNull()
+            .Matches("((TABLE)|(QUICK)|(BAR))\\d{2,4}_\\d{1,2}:")
+            .WithMessage("The terminal name is in the correct format. Please correct the issue and try again.");
+
+        RuleFor(x => x.IpAddress)
+            .NotNull()
+            .Matches("((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
+            .WithMessage("The IP Address is not in the correct format. Please try again.");
+    }
+}
