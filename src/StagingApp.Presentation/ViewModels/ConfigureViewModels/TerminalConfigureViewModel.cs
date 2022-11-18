d853343cdec44d7514cf8e3fae54fe741b9415dd
Harlan Siegel <hmsiegel79@msn.com>
2022-11-18 15:46:04 -0500

    Skeleton Out TerminalConfigureViewModel

--- src/StagingApp.Presentation/ViewModels/ConfigureViewModels/TerminalConfigureViewModel.cs
+++ src/StagingApp.Presentation/ViewModels/ConfigureViewModels/TerminalConfigureViewModel.cs
@@ -1,14 +1,26 @@
-﻿namespace StagingApp.Presentation.ViewModels.ConfigureViewModels;
+﻿using MediatR;
+
+using StagingApp.Application.FileSystem.Queries.CheckMarkerFile;
+using StagingApp.Domain;
+using StagingApp.Domain.Shared;
+
+namespace StagingApp.Presentation.ViewModels.ConfigureViewModels;
 public sealed class TerminalConfigureViewModel : BaseConfigureViewModel
 {
     private static readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
+    private readonly IValidator<TerminalConfigureModel> _validator;
+    private readonly ISender _sender;
+
     private string? _terminalName;
     private string? _ipAddress;
 
-    public TerminalConfigureViewModel()
+    public TerminalConfigureViewModel(
+        IValidator<TerminalConfigureModel> validator,
+        ISender sender)
     {
-
+        _validator = validator;
+        _sender = sender;
     }
 
     protected override void OnViewLoaded(object view)
@@ -41,7 +53,20 @@ public sealed class TerminalConfigureViewModel : BaseConfigureViewModel
 
     public override void ValidateInput()
     {
-        throw new NotImplementedException();
+        _logger.Info("Validating user inputs...");
+
+        var model 
+            = TerminalConfigureModel.Create(
+                TerminalName!,
+                IpAddress!);
+
+        var result = _validator.Validate(model);
+
+        if (result.IsValid)
+        {
+            _logger.Info("Inputs are valid. Configuring...");
+            SaveTerminalInfoAndSysPrep(model);
+        }
         // TODO: Implement data input validation
     }
 
@@ -49,5 +74,35 @@ public sealed class TerminalConfigureViewModel : BaseConfigureViewModel
     {
         throw new NotImplementedException();
         // TODO: Implement the configure method to start the configuration
+    }
+    private async void CheckForMarkerFiles()
+    {
+        _logger.Info("Checking for marker files...");
+
+        var markerFile =
+            Directory.GetFiles(GlobalConfig.ScriptPath, "*.done", SearchOption.TopDirectoryOnly).FirstOrDefault();
+
+        if (markerFile is null)
+        {
+            _logger.Info("No marker files found. Starting first run...");
+            StartOSK();
+        }
+
+        CheckMarkerFileQuery query = new CheckMarkerFileQuery(markerFile!);
+
+        Result<string> response = await _sender.Send(query, new CancellationToken());
+
+        switch (response.Value)
+        {
+            case nameof(MarkerFiles.first):
+                StartStageTerminal();
+                break;
+            case nameof(MarkerFiles.second):
+                StartThirdPass();
+                break;
+            case nameof(MarkerFiles.third):
+                StartRadiantAutoLoader();
+                break;
+        }
     }
 }
