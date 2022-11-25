5ac87763d83cf89aea5f8f898a9ff579f1750564
Harlan Siegel <hmsiegel79@msn.com>
2022-11-24 21:12:13 -0500

    Add Test Projects

---
+++ test/StagingApp.Application.UnitTest/Shell/Queries/DetermineDevice/DetermineDeviceQueryHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+
+using StagingApp.Application.Abstractions.Services;
+using StagingApp.Application.Shell.Queries.DetermineDevice;
+
+namespace StagingApp.Application.UnitTest.Shell.Queries.DetermineDevice;
+public class DetermineDeviceQueryHandlerTests
+{
+    private readonly Mock<IDeviceTypeService> _deviceServiceMock;
+
+    public DetermineDeviceQueryHandlerTests(Mock<IDeviceTypeService> deviceServiceMock)
+    {
+        _deviceServiceMock = deviceServiceMock;
+    }
+
+    [Fact]
+    public async Task Handler_ShouldReturnFailure_WhenDeviceTypeIsNotKnown()
+    {
+        // Arrange
+        var query = new DetermineDeviceQuery();
+
+        // Act
+
+        // Assert
+    }
+}
